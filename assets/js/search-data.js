bb2b16635de4278f6c6feef09f9bb1f0d58b5b2c
tsha-256 <102328408+tsha-256@users.noreply.github.com>
2025-12-03 16:12:27 +0000

    Deploying to gh-pages from @ tsha-256/tsha-256.github.io@8a6c03743ff586f574d54f146b9111f01b950a32 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/news/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of projects.",
